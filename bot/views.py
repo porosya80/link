3547a29b83759732514170d7ac9c5263b330e934
Dmytriy Myk <porosya@gmail.com>
2019-06-30 01:44:11 +0300

    fix send links

--- bot/views.py
+++ bot/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpResponse
 from django.conf import settings
+from links.models import Link
 import requests
 
 
 def index(request):
     id = "137199697"
-    bot_message = "[link](gmail.com)"
-    send_text = (
-        "https://api.telegram.org/bot"
-        + settings.BOT_TOKEN
-        + "/sendMessage?chat_id="
-        + id
-        + "&parse_mode=Markdown&text="
-        + bot_message
-    )
 
-    response = requests.get(send_text)
+    q = Link.objects.filter(sended=False)
+
+    for link in q:
+        link.sended = True
+        link.save()
+
+        bot_message = f"[{link.text}]({link.text})"
+        send_text = (
+            "https://api.telegram.org/bot"
+            + settings.BOT_TOKEN
+            + "/sendMessage?chat_id="
+            + id
+            + "&parse_mode=Markdown&text="
+            + bot_message
+        )
+        response = requests.get(send_text)
 
     return HttpResponse(response.json())
+    # return HttpResponse(response)
--- links/viewsets.py
+++ links/viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Link
 from .serializers import LinkSerializer
+
+
 class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
