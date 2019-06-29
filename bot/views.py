8a169879a040dbb05eb37561333cccdd8eca4f55
Dmytriy Myk <porosya@gmail.com>
2019-06-30 01:11:29 +0300

    bot is working

--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from django.conf import settings
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello, world. This is the bot app.")
+    id = "137199697"
+    bot_message = "[link](gmail.com)"
+    send_text = (
+        "https://api.telegram.org/bot"
+        + settings.BOT_TOKEN
+        + "/sendMessage?chat_id="
+        + id
+        + "&parse_mode=Markdown&text="
+        + bot_message
+    )
+
+    response = requests.get(send_text)
+
+    return HttpResponse(response.json())
