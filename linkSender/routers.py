b44326d120688da2aeba802e901374f9f0fc1033
Dmytriy Myk <porosya@gmail.com>
2019-06-29 17:10:38 +0300

    rest api worked

--- linkSender/routers.py
+++ linkSender/routers.py
@@ -1,4 +1,5 @@
 from rest_framework import routers
 from links.viewsets import LinkViewSet
+
 router = routers.DefaultRouter()
-router.register(r'link', LinkViewSet)
+router.register(r"link", LinkViewSet)
